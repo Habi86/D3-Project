7b1643d3d88b8accabf51f08b3b8ca10819b6e8f
Vera KARL <vkarl.mmt-b2014@fh-salzburg.ac.at>
2017-12-19 13:10:14 +0100

    setup updated eslint file

--- webpack.config.js
+++ webpack.config.js
@@ -1,57 +1,55 @@
-const path = require('path')
-const webpack = require('webpack')
+const path = require('path');
+const webpack = require('webpack');
 
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
-const CopyWebpackPlugin = require('copy-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const paths = {
-  src: path.join(__dirname, 'src'),
-  dist: path.join(__dirname, 'dist'),
-  data: path.join(__dirname, 'data')
-}
+    src: path.join(__dirname, 'src'),
+    dist: path.join(__dirname, 'dist'),
+    data: path.join(__dirname, 'data')
+};
 
 module.exports = {
-  context: paths.src,
-  entry: ['./app.js', './main.scss'],
-  output: {
-    filename: 'app.bundle.js',
-    path: paths.dist,
-    publicPath: 'dist',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: [/node_modules/],
-        use: [{
-          loader: 'babel-loader',
-          options: { presets: ['es2015', 'stage-0'] },
+    context: paths.src,
+    entry: ['./js/app.js', './scss/main.scss'],
+    output: {
+        filename: 'app.bundle.js',
+        path: paths.dist,
+        publicPath: 'dist',
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: [/node_modules/],
+            use: [{
+                loader: 'babel-loader',
+                options: {
+                    presets: ['es2015', 'stage-0']
+                },
+            }],
+        },
+        {
+            test: /\.scss$/,
+            use: ExtractTextPlugin.extract([
+                'css-loader', 'sass-loader'
+            ]),
         }],
-      },
-      {
-        test: /\.scss$/,
-        use: ExtractTextPlugin.extract([
-          'css-loader', 'sass-loader'
-        ]),
-      }
+    },
+    devServer: {
+        contentBase: paths.dist,
+        compress: true,
+        port: '4800',
+        stats: 'errors-only',
+    },
+    plugins: [
+        new ExtractTextPlugin({
+            filename: 'main.bundle.css',
+            allChunks: true,
+        }),
+        new CopyWebpackPlugin([{
+            from: paths.data,
+            to: paths.dist + '/data'
+        }])
     ],
-  },
-  devServer: {
-    contentBase: paths.dist,
-    compress: true,
-    port: '4800',
-    stats: 'errors-only',
-  },
-  plugins: [
-    new ExtractTextPlugin({
-      filename: 'main.bundle.css',
-      allChunks: true,
-    }),
-    new CopyWebpackPlugin([
-      {
-        from: paths.data,
-        to: paths.dist + '/data'
-      }
-    ])
-  ],
-}
+};
