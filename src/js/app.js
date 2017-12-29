928b2065bbae9d043bbffb94daec6a550b546cdb
Stefanie Habersatter <habi1986@gmx.at>
2017-12-29 19:33:38 +0100

    Adding some BubbleChart progress

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,20 @@
 import * as d3 from 'd3';
+import { bubbleChart } from './bubble';
 
+
+//Check if d3 is working properly :) TODO: REMOVE ME
 // Selecting and appending elements
-d3.select('#root')
-    .append('h5')
-    .append('text')
-    .text(`D3 version: ${d3.version}`);
+// d3.select('#root')
+//     .append('h5')
+//     .append('text')
+//     .text(`D3 version: ${d3.version}`);
 
-// Loading external data
-d3.csv('/data/stackoverflow_haveWorkedLanguage.csv', (error, dataset) => {
-    dataset.forEach((data) => { console.log(data)})
-});
+//Loading external data
+// d3.csv('/data/stackoverflow_haveWorkedLanguage.csv', (error, dataset) => {
+//     //dataset.forEach((data) => { console.log(data)})
+// });
+
+
+
+//BUBBLECHART
+bubbleChart();
