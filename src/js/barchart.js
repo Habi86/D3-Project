0220a10a40ea6121a1eec2c5e848a454e0796533
Vera KARL <vkarl.mmt-b2014@fh-salzburg.ac.at>
2018-01-01 19:01:58 +0100

    some transitions and effects - updated labels

--- src/js/barchart.js
+++ src/js/barchart.js
@@ -2,6 +2,8 @@ import * as d3 from 'd3';
 //import * as _ from 'underscore';
 var surveyResults;
 let devsPerCountry;
+let clicked = false;
+
 
 function barChart() {
 
@@ -10,13 +12,15 @@ function barChart() {
             console.error(error);
         }
         else {
-            surveyResults = csv.splice(0, 10);
+            surveyResults = csv;
             // group devs by country
             devsPerCountry = d3.nest()
                 .key(function(d) {
                     return d.Country;
                 })
                 .entries(surveyResults);
+            // filter only data over  a certain amount of devs 
+            devsPerCountry = devsPerCountry.filter(greaterThanEdgeValue);
             console.log(devsPerCountry);
         }
         updateChart(devsPerCountry);
@@ -24,12 +28,13 @@ function barChart() {
 
     const margin = {
         top: 40,
-        bottom: 10,
-        left: 120,
+        bottom: 40,
+        left: 200,
         right: 20
     };
+
     const width = 800 - margin.left - margin.right;
-    const height = 600 - margin.top - margin.bottom;
+    const height = 1500 - margin.top - margin.bottom;
 
     // Creates sources <svg> element
     const svg = d3.select('body').append('svg')
@@ -40,22 +45,37 @@ function barChart() {
     const g = svg.append('g')
         .attr('transform', `translate(${margin.left},${margin.top})`);
 
-    // Scales setup
+    // setup for the scales
     const xscale = d3.scaleLinear().range([0, width]);
     const yscale = d3.scaleBand().rangeRound([0, height]).paddingInner(0.5).paddingOuter(0.2);
 
-    // Axis setup
+    // setup for the axis
     const xaxis = d3.axisTop().scale(xscale);
     const gXaxis = g.append('g').attr('class', 'x axis');
     const yaxis = d3.axisLeft().scale(yscale);
     const gYaxis = g.append('g').attr('class', 'y axis');
 
+    // add label for x-axis
+    svg.append('text')
+        .attr('x', 400)
+        .attr('y', 10)
+        .attr('class', 'x-axis')
+        .text('Amount of developers');     
+
+    // add label for y-axis
+    svg.append('text')
+        .attr('x', -500)
+        .attr('y', 60)
+        .attr('transform', 'rotate(-90)')
+        .attr('class', 'y-axis') 
+        .text('Countries');     
+
     // update the chart
     function updateChart(devsPerCountry) {
     
         //update the scales
-        xscale.domain([0, surveyResults.length]); // to amount of devs (length of survey arr)
-        yscale.domain(surveyResults.map((d) => d.Country));
+        xscale.domain([0, 14000]); // to amount of devs (length of survey arr)
+        yscale.domain(devsPerCountry.map((d) => d.key));
         
         //render the axis
         gXaxis.call(xaxis);
@@ -70,16 +90,29 @@ function barChart() {
             .append('rect')
             .attr('x', 0);
         rectEnter.append('title');
+    
+        rect.merge(rectEnter).select('title').text((d) => `${d.values.length} developers in ${d.key}`);
         
-        // ENTER + UPDATE
-        // both old and new elements
-        rect.merge(rectEnter)
-            .attr('height', yscale.bandwidth())
-            .attr('width', (d) => xscale(d.values.length)) // amount of devs per country 
-            .attr('y', (d) => yscale(d.key));
+        if (clicked) {
+            // ENTER + UPDATE
+            // only if filter is set
+            rect.merge(rectEnter)
+                .transition()
+                .duration(3000)
+                .style('fill', 'red')
+                .attr('height', yscale.bandwidth())
+                .attr('width', (d) => xscale(d.values.length)) // amount of devs per country 
+                .attr('y', (d) => yscale(d.key));
+        }
+        else {
+            // ENTER + UPDATE
+            // only if filter is not set
+            rect.merge(rectEnter)
+                .attr('height', yscale.bandwidth())
+                .attr('width', (d) => xscale(d.values.length)) // amount of devs per country 
+                .attr('y', (d) => yscale(d.key));
 
-        rect.merge(rectEnter).select('title').text((d) => d.values.length);
-        
+        }
         // EXIT
         // elements that aren't associated with data
         rect.exit().remove();
@@ -88,9 +121,11 @@ function barChart() {
     // filter possibility
     // filter for highest amount of devs
     d3.select('#filter-highest').on('change', function() {
+        clicked = true;
         const checked = d3.select(this).property('checked');
         if (checked === true) {
             filterData(true); // update with highest amount of devs
+            clicked = false;
         }
         else {
             updateChart(devsPerCountry);  // Update with all data
@@ -99,9 +134,11 @@ function barChart() {
 
     // filter for lowest amount of devsl
     d3.select('#filter-lowest').on('change', function() {
+        clicked = true;
         const checked = d3.select(this).property('checked');
         if (checked === true) {
             filterData(false); // update with lowest amount of devs
+            clicked = false;
         }
         else {
             updateChart(devsPerCountry);  // Update with all data
@@ -110,10 +147,19 @@ function barChart() {
 
     function filterData(max) {
         const filterLengths = devsPerCountry.map((d) => d.values.length);
-        let edgeVal = (max) ? Math.max(...filterLengths): Math.min(...filterLengths);
+        let edgeVal = (max) ? Math.max(...filterLengths) : Math.min(...filterLengths);
         const filterData = devsPerCountry.filter(d => d.values.length === edgeVal);
         updateChart(filterData);
     }
+
+    function greaterThanEdgeValue(data) {
+        return data.values.length > 140; 
+        // used 140 as edgevalue (10% of maximum) as we have 14.000 as the maximum 
+        // amount of devs in a country 
+        // we had more than 200 countries which was not really useful in 
+        // the diagram to show so we reduced it 
+    }
+    
 }
 export { barChart };
 
