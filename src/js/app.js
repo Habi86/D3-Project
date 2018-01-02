f87d97a78f9ae05333d7bf2a8c76894537219bd6
Stefanie Habersatter <habi1986@gmx.at>
2018-01-02 16:40:06 +0100

    Base 4 Parallel Set (from linking)

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,5 @@
 import * as d3 from 'd3';
-import * as _ from 'underscore';
+//import * as _ from 'underscore';
 import { bubbleChart } from './bubbleChart';
 import { barChart } from './barchart';
 
