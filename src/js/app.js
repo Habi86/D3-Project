1169011ec9160b774f47fc342b86c27d9a68478f
Stefanie Habersatter <habi1986@gmx.at>
2018-01-05 10:42:00 +0100

    Setup for Parallel Sets

--- src/js/app.js
+++ src/js/app.js
@@ -1,20 +1,19 @@
 import BubbleChart from './bubbleChart';
 import Barchart from './barChart';
-import ParallelSet from './parallelSet';
+import ParallelSet from './parallel';
 
 
 // call charts and listen for change events
-Barchart.onChange((newValue) => {
-    BubbleChart.setCountry(newValue);
-    console.log('onChangeBarchart',newValue);
-});
-
-BubbleChart.onChange((newValue) => {
-    Barchart.setCountry(newValue);
-    console.log('onChangeBubblechart',newValue);
-});
-
 ParallelSet.onChange((newValue) => {
     console.log('onChangeParallelSet',newValue);
 });
 
+// Barchart.onChange((newValue) => {
+//     BubbleChart.setCountry(newValue);
+//     console.log('onChangeBarchart',newValue);
+// });
+//
+// BubbleChart.onChange((newValue) => {
+//     Barchart.setCountry(newValue);
+//     console.log('onChangeBubblechart',newValue);
+// });
